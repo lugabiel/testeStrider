6064ef896d8a6f9761ae8cb365cedc66d453c957
lugabiel <36977837+lugabiel@users.noreply.github.com>
2019-05-29 19:17:42 -0300

    solucao 0.1.1 A lógica para procurar os pixels é um for duplo(v0.1). É necessário fazer uma comparacao bit a bit com o bit vermelho [255, 0, 0]. Para fazer essa comaparação tem que usar o pillow+numpy ou o openCV.

--- testeS.py
+++ testeS.py
@@ -1,12 +1,30 @@
 from PIL import Image 
+from PIL import ImageChops as chops
 import numpy as np
+import cv2
 
-
-img = Image.open(r"C:\Users\João\Desktop\Teste Strider\challenge_strider.png")
-imgPubli = Image.open(r"C:\Users\João\Desktop\Teste Strider\imgDominiopublico.jpg")
+img = Image.open(r"C:\Users\João\Desktop\Teste Strider\testeStriver\challenge_strider.png")
+imgPubli = Image.open(r"C:\Users\João\Desktop\Teste Strider\testeStriver\vermelho.png")
 print('imagem convertida')
-print(img)
 
+out = chops.add(img , imgPubli)
+
+print('matriz diferenca')
+print(out)
+out.save('diferenca.png')
+
+imgCV = cv2.imread(r"C:\Users\João\Desktop\Teste Strider\testeStriver\challenge_strider.png", cv2.IMREAD_UNCHANGED)
+print(imgCV)
+for i in range(0 , 663):
+    print(i,'esia linha########################################################################################3')
+    for j in range(0, 663):
+        px = imgCV[i,j] 
+        if imgCV[i,j] == '[255, 0, 0]':
+            print(imgM[i,j])
+        
+
+
+'''
 
 imgM = np.asarray(img)
 
@@ -25,21 +43,25 @@ j = 0
 i = 0
 print(imgM.shape)
 tam =imgM.shape[0]*imgM.shape[1]
-
 '''
-for i in range(0 , 663 ):
+'''
+for i in range(0 , 663):
+    print(i,'esia linha########################################################################################3')
     for j in range(0, 663):
+        #if imgM[i,j] == '[255, 0, 0]'
         print(imgM[i,j])
-''' 
+        
 
 redP = 0
-
-
+'''
+'''
 for i in imgM:
-    if i.all() != 255:
-        redP += 1
-        print(imgM[i,j])
-        #break
-        print(redP)
+    for j in imgM:
+        if imgM[98,68][1]==255:    
+            redP += 1
+            print(imgM[i,j])
+            #break
+            print(redP)
 
 print (redP)
+'''
