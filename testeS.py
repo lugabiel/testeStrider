668f91be2a0377a55b34d06acafc24540ed6ae46
lugabiel <36977837+lugabiel@users.noreply.github.com>
2019-05-30 18:21:35 -0300

    solucao 1.0 - A solução que encontra pixels vermelhos no formato [b,g,r], a imagem 'challenge_strider.png' deve estar no mesmo diretorio que o arquivo 'solucaoFinal.py'.

--- testeS.py
+++ testeS.py
@@ -7,22 +7,54 @@ img = Image.open(r"C:\Users\João\Desktop\Teste Strider\testeStriver\challenge_s
 imgPubli = Image.open(r"C:\Users\João\Desktop\Teste Strider\testeStriver\vermelho.png")
 print('imagem convertida')
 
-out = chops.add(img , imgPubli)
+#out = chops.logical_and(img , imgPubli)
+out = ((img and imgPubli))
 
 print('matriz diferenca')
 print(out)
 out.save('diferenca.png')
 
-imgCV = cv2.imread(r"C:\Users\João\Desktop\Teste Strider\testeStriver\challenge_strider.png", cv2.IMREAD_UNCHANGED)
+'''lê imagem .png'''
+imgCV = cv2.imread("challenge_strider.png", -1)
 print(imgCV)
-for i in range(0 , 663):
-    print(i,'esia linha########################################################################################3')
-    for j in range(0, 663):
-        px = imgCV[i,j] 
-        if imgCV[i,j] == '[255, 0, 0]':
-            print(imgM[i,j])
-        
+pxBranco = imgCV[0,0]
 
+print(pxBranco.all(),pxBranco.any())
+
+
+'''le vermelho.npg'''
+#vermelho
+pxVermelho = imgCV[2,574]
+print(pxVermelho.all(),pxVermelho.any())
+print(imgCV.all(),imgCV.any())
+
+#acessando cores do pixel
+azul = pxVermelho[0]
+verde = pxVermelho[1]
+vermelho = pxVermelho[2]
+
+print(pxVermelho)
+print(vermelho, verde, azul)
+
+redP = 0
+print('qnt de pixels vermelhos: ',redP)
+
+input('Pressione enter para continuar') 
+for i in range(-1 , 664): #colunas matriz de pixels
+    for j in range(-1, 664): #linhas matriz de pixels
+        pixel = imgCV[i,j] 
+        azulComp= pixel[0]
+        verdeComp = pixel[1]
+        vermelhoComp = pixel[2]
+        
+        '''Verifica se o pixel é vermelho'''
+        if azulComp == azul and verdeComp == verde and vermelhoComp == vermelho : 
+        #if pixel.all() != pxVermelho.any():
+            print(pixel,'pixel [', i,j,']')
+            redP += 1
+        pass
+
+print('qnt de pixels vermelhos: ',redP)
 
 '''
 
